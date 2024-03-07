80b2c247d08a7a1424b38fda4e80987135f5a28b
djordjesaric493 <djordjesaric1999@gmail.com>
2024-03-07 12:51:02 +0100

    firebase setup (successfully)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firechat/firebase_options.dart';
 import 'package:firechat/theme/light_mode.dart';
 import 'package:flutter/material.dart';
 
 //instead of separate imports for login and register
 import 'package:firechat/app_components/auth/lor_or_reg.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
